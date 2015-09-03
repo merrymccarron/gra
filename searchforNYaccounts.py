d729195b0b1c66b83d5c599be22cae4efe62b86a
Merry McCarron <mmccarron@gmail.com>
2015-09-03 17:39:22 -0400

    added comments and cleaned up code for next batch of GRAs

--- searchforNYaccounts.py
+++ searchforNYaccounts.py
@@ -1,3 +1,8 @@
+'''
+This script takes a search term (like "Bronx") and returns the username 
+results of a call to Twitter's Search API.
+Documentation: https://dev.twitter.com/rest/reference/get/users/search
+'''
 import sys
 import os
 import pandas as pd
@@ -7,12 +12,14 @@ import time
 import datetime
 import string
 
-CONSUMER_KEY='FqjFRT1OHl6xyIGoq9uXSA'
-CONSUMER_SECRET='KuhoVREmf7ngwjOse2JOLJOVXNCi2IVEzQZu2B8'
-ACCESS_TOKEN='114454541-xcjy2sbl7Rr4oIaogsaBrlVL5H4CvcdvOSMy3MnR'
-ACCESS_TOKEN_SECRET='yyBBOJhxgfw9pezZda2hWF94doONSd50y0JoylYjL3rmY'
+#Best practice is to NOT keep API keys in public GitHub repos!
+APIKEYS = pd.read_json('cussacAPIKeys.json')
+CONSUMER_KEY = (APIKEYS['CONSUMER_KEY'].values)[0]
+CONSUMER_SECRET = (APIKEYS['CONSUMER_SECRET'].values)[0]
+ACCESS_TOKEN = (APIKEYS['ACCESS_TOKEN'].values)[0]
+ACCESS_TOKEN_SECRET = (APIKEYS['ACCESS_TOKEN_SECRET'].values)[0]
 
-
+#Setting up API authentication
 def oauth_req(url, http_method="GET", post_body='', http_headers=None):
     consumer = oauth.Consumer(key=CONSUMER_KEY, secret=CONSUMER_SECRET)
     token = oauth.Token(key=ACCESS_TOKEN, secret=ACCESS_TOKEN_SECRET)
@@ -26,23 +33,24 @@ def oauth_req(url, http_method="GET", post_body='', http_headers=None):
     return content
 
 
-# URL = 'https://api.twitter.com/1.1/users/search.json?q=New+York+City&page='+ pagecount +'&count=20'
-
+# example URL = 'https://api.twitter.com/1.1/users/search.json?q=New+York+City&page='+ pagecount +'&count=20'
 def get_nyc_accounts(searchterm):
     data = {'UserID' : [], 'location' : [], 'screen_name' : [], 'name': [], 'geo_enabled': [], 
         'followers_count':[], 'verified':[], 'time_zone': [], 'geo': [], 'statuses_count': [],
         'description': []}
     df = pd.DataFrame(data)
+    #this checks if a file already exists for this searchterm and creates one if it doesn't exist
     if not os.path.isfile(searchterm + 'Searchuserdetails.csv'):
         df.to_csv(searchterm + 'Searchuserdetails.csv', header = True, columns = ['UserID', 
             'location', 'screen_name', 'name', 'geo_enabled', 'followers_count', 'verified', 
             'time_zone', 'geo', 'statuses_count', 'description'], engine='python')
-
     for i in range(50):
-        #this api is limited to the first 1000 results
+        #this api is limited to the first 1000 results with a maximum of 20 results per call
         URL = 'https://api.twitter.com/1.1/users/search.json?q=' + searchterm + '&page='+ 
             str(i) +'&count=20'
         try:
+            #try-except-else is the PEP-8 standard for try statements, for explanation see:
+            #https://www.python.org/dev/peps/pep-0008/#programming-recommendations
             data = oauth_req(URL)
         except:
             print "error after " + str(i) + "calls, exiting program at:"
@@ -52,23 +60,27 @@ def get_nyc_accounts(searchterm):
             break
         else:
             tempdf = pd.DataFrame(data=data)
+            #because of the if not...: statement creating file, this can always be opened in 
+            #append mode ('a')
             with open(searchterm + 'Searchuserdetails.csv', 'a') as f:
-                    tempdf['name'] = tempdf['name'].apply(lambda x: x.encode('ascii', 'ignore'))
-                    tempdf['location'] = tempdf['location'].apply(lambda x: x.encode('ascii', 'ignore'))
-                    tempdf['description'] = tempdf['description'].apply(lambda x: x.encode('ascii', 'ignore'))
-                    tempdf.to_csv(f, header=False, columns = ['id_str', 'location', 'screen_name', 
-                        'name', 'geo_enabled', 'followers_count', 'verified', 'time_zone', 'geo', 
-                        'statuses_count', 'description'], engine='python')#, encoding = 'utf-8')
-            
+            #using "with open..." saves the trouble of having to put a close() statement after 
+            #opening file
+                tempdf['name'] = tempdf['name'].apply(lambda x: x.encode('ascii', 'ignore'))
+                tempdf['location'] = tempdf['location'].apply(lambda x: x.encode('ascii', 'ignore'))
+                tempdf['description'] = tempdf['description'].apply(lambda x: x.encode('ascii', 'ignore'))
+                tempdf.to_csv(f, header=False, columns = ['id_str', 'location', 'screen_name', 
+                    'name', 'geo_enabled', 'followers_count', 'verified', 'time_zone', 'geo', 
+                    'statuses_count', 'description'], engine='python')#, encoding = 'utf-8')
+            #debugging statements
             print "number of API calls:" + str(i)
             print 'sleeping now'
+            #free API rate limit is one call per 5 seconds.
             time.sleep(5)
 
 
 def main():
-    # if len(sys.argv) != 2:
-    #     print 'please specify a search term'
-    # username = sys.argv[1]
+    if len(sys.argv) != 2:
+        print 'please specify a search term'
     searchterm = sys.argv[1]
     get_nyc_accounts(searchterm)
 
